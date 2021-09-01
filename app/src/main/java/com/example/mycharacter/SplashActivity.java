03022ccb89ce6c8d29bf56a322d484023122074c
lu-luma <lumaluizalua@gmail.com>
2021-08-31 22:26:11 -0300

    -splash screen

---
+++ app/src/main/java/com/example/mycharacter/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.example.mycharacter;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.splash_activity);
+    }
+}
